676e42233ad5eee4e730954774356bdf30bc87dd
vickybiswas <vickybiswas@gmail.com>
2025-04-25 15:03:59 +0530

    Adding out folder

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,16 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
 
 const isProd = process.env.NODE_ENV === 'production';
 
-    module.exports = {
-      output: 'export',
-      basePath: isProd ? '/vignesh' : '', // Replace <repo-name> with your repository name
-      assetPrefix: isProd ? '/vignesh/' : '', // Replace <repo-name> with your repository name
-      images: {
-        unoptimized: true,
-      },
-    };
+const nextConfig: NextConfig = {
+  /* config options here */
+    output: 'export',
+    basePath: isProd ? '/vignesh' : '', // Replace <repo-name> with your repository name
+    assetPrefix: isProd ? '/vignesh/' : '', // Replace <repo-name> with your repository name
+    images: {
+    unoptimized: true,
+    },
+};
 
     export default nextConfig;
