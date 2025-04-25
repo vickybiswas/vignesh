fc1fadcb1bc666352e8b8947abc3984c4c5753a2
vickybiswas <vickybiswas@gmail.com>
2025-04-25 05:38:22 +0530

    Configuration Changes Prepping for github

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,15 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+const isProd = process.env.NODE_ENV === 'production';
+
+    module.exports = {
+      output: 'export',
+      basePath: isProd ? '/vignesh' : '', // Replace <repo-name> with your repository name
+      assetPrefix: isProd ? '/vignesh/' : '', // Replace <repo-name> with your repository name
+      images: {
+        unoptimized: true,
+      },
+    };
+
+    export default nextConfig;
