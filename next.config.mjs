056bca60e0d812431c744db126cb21343f2fa4ac
vickybiswas <vickybiswas@gmail.com>
2025-04-30 02:49:34 +0530

    GHPages config 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
+
 const nextConfig = {
+    output: 'export',
+    basePath: isProd ? '/vignesh' : '', // Replace <repo-name> with your repository name
+    assetPrefix: isProd ? '/vignesh/' : '', // Replace <repo-name> with your repository name
   eslint: {
     ignoreDuringBuilds: true,
   },
